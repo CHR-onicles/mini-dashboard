76b54f179dd2436261b29189dadea7b5e7411b64
CHR-onicles <tpandivine48@gmail.com>
2022-08-04 11:55:38 +0000

    Added react transition group

--- src/App/App.styled.ts
+++ src/App/App.styled.ts
@@ -35,5 +35,29 @@ export const StyledApp = styled.div`
     ${mq(bp.xxlarge)} {
       flex: 4.5;
     }
+
+
   }
+
+  .fade-enter {
+      opacity: 0;
+      transform: scale(1.1);
+    }
+
+    .fade-enter-active {
+      opacity: 1;
+      transform: scale(1);
+      transition: opacity 300ms, transform 300ms;
+    }
+
+    .fade-exit {
+      opacity: 1;
+      transform: scale(1);
+    }
+
+    .fade-exit-active {
+      opacity: 0;
+      transform: scale(0.9);
+      transition: opacity 300ms, transform 300ms;
+    }
 `;
--- src/App/App.tsx
+++ src/App/App.tsx
@@ -1,3 +1,11 @@
+import {
+  BrowserRouter as Router,
+  Route,
+  Routes,
+  useLocation,
+} from "react-router-dom";
+import { CSSTransition, TransitionGroup } from "react-transition-group";
+
 import { GlobalStyles } from "../styles/GlobalStyles.styled";
 import { Navbar } from "../components";
 import { Sidebar } from "../components";
@@ -5,38 +13,49 @@ import { StyledApp } from "./App.styled";
 // import { Preloader } from "../components/Preloader";
 import { useState } from "react";
 import { Home } from "../pages/Home";
-import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
 import { UserList } from "../pages/UserList";
 import { User } from "../pages/User";
 import { NewUser } from "../pages/NewUser";
 
-export const App = () => {
+const App = () => {
   const [isSideMenuOpen, setIsSideMenuOpen] = useState(false);
+  const location = useLocation();
+
   return (
-    <Router>
-      <StyledApp>
-        {/* <Preloader /> */}
-        <GlobalStyles />
-        <Navbar
+    <StyledApp>
+      {/* <Preloader /> */}
+      <GlobalStyles />
+      <Navbar
+        isSideMenuOpen={isSideMenuOpen}
+        setIsSideMenuOpen={setIsSideMenuOpen}
+      />
+      <div className="app-container">
+        <Sidebar
+          className="sidebar"
           isSideMenuOpen={isSideMenuOpen}
           setIsSideMenuOpen={setIsSideMenuOpen}
         />
-        <div className="app-container">
-          <Sidebar
-            className="sidebar"
-            isSideMenuOpen={isSideMenuOpen}
-            setIsSideMenuOpen={setIsSideMenuOpen}
-          />
-          <Routes>
-            <Route path='/' element={<Home className="page" />}/>
-            <Route path='/users' element={<UserList className="page" />}/>
-            <Route path='/user/:id' element={<User className="page" />}/>
-            <Route path='/newUser' element={<NewUser className="page" />}/>
-            <Route path='*' element={<div className="page">Error page</div>}/>
-          </Routes>
-          
-        </div>
-      </StyledApp>
-    </Router>
+        <TransitionGroup component={null}>
+          <CSSTransition key={location.key} classNames={"fade"} timeout={300}>
+            <Routes location={location}>
+              <Route path="/" element={<Home className="page" />} />
+              <Route path="/users" element={<UserList className="page" />} />
+              <Route path="/user/:id" element={<User className="page" />} />
+              <Route path="/newUser" element={<NewUser className="page" />} />
+              <Route
+                path="*"
+                element={<div className="page">Error page</div>}
+              />
+            </Routes>
+          </CSSTransition>
+        </TransitionGroup>
+      </div>
+    </StyledApp>
   );
 };
+
+export const RootApp = () => (
+  <Router>
+    <App />
+  </Router>
+);
